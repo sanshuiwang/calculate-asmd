9018d8ee3ed578247f7152f9b47da5e077513e81
wangyang <768188667@qq.com>
2019-12-21 17:26:30 +0800

    typings

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+export const scaleNum: (number: number, position: number) => number
+export const add: (arg1: number, arg2: number) => number
+export const sub: (arg1: number, arg2: number) => number
+export const mul: (arg1: number, arg2: number) => number
+export const div: (arg1: number, arg2: number) => number
